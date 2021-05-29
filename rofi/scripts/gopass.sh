97628ddfcad3f5b61a70bbc402b8c040f299d1e6
Thomas Mueller <11089865+tomsaen@users.noreply.github.com>
2021-05-29 15:04:14 +0200

    update configs

--- rofi/scripts/gopass.sh
+++ rofi/scripts/gopass.sh
@@ -1,1 +1,2 @@
 gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass show -c
+# gopass ls --flat | rofi -dmenu -p gopass | xargs --no-run-if-empty gopass show -f | head -n 1 | ydotool type --clearmodifiers --file -
